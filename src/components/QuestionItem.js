3d1e59f7884a88eab8a63ebb1c653427755cb2b1
Khadaryussuf <salatkhadar@gmail.com>
2023-01-23 08:09:55 +0300

    Solved

--- src/components/QuestionItem.js
+++ src/components/QuestionItem.js
@@ -1,7 +1,10 @@
-import React from "react";
 
-function QuestionItem({ question }) {
+import React, { useState } from "react";
+
+
+function QuestionItem({ question, onDeleteQuestion }) {
   const { id, prompt, answers, correctIndex } = question;
+  const [updatedIndex, setUpdatedIndex] = useState(correctIndex)
 
   const options = answers.map((answer, index) => (
     <option key={index} value={index}>
@@ -9,15 +12,30 @@ function QuestionItem({ question }) {
     </option>
   ));
 
+  function handleOnChange(event){
+    let index = event.target.id
+    setUpdatedIndex(index)
+    fetch(`http://localhost:4000/questions/${id}`, {
+      method:"PATCH", 
+      headers:{
+        "Content-Type":"application/json"
+      }, 
+      body:JSON.stringify({"correctIndex": index})
+    })
+    .then((response) => response.json())
+    .then((index) => setUpdatedIndex(index))
+
+  }
+
   return (
     <li>
       <h4>Question {id}</h4>
       <h5>Prompt: {prompt}</h5>
       <label>
         Correct Answer:
-        <select defaultValue={correctIndex}>{options}</select>
+        <select defaultValue={updatedIndex} onChange={handleOnChange}>{options}</select>
       </label>
-      <button>Delete Question</button>
+      <button onClick={() => onDeleteQuestion(id)}>Delete Question</button>
     </li>
   );
 }
